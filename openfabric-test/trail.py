f283d9cc1bade076f621fce6fce52d14d75a142c
iyash <iyashsrivastav@gmail.com>
2023-03-15 23:39:51 +0530

    merger folder

--- openfabric-test/trail.py
+++
@@ -1,8 +0,0 @@
-import wolframalpha
-query='what'
-if 'what' in query or 'solve' in query:
-  app_id = ['GE8LH7-7YTEQWAGE2']
-  client = wolframalpha.Client(app_id)
-  res = client.query(query)
-  answer = next(res.results).text
-  print(answer)
